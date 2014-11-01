5aed2a6956d0aa2171b6681e9d08f478557d9926
aoricm <aoricm@aol.com>
2014-11-01 13:08:41 +1100

    text

--- text.rb
+++ text.rb
@@ -1,24 +1,24 @@
-path = File.expand_path File.dirname(__FILE__)
-if File.exists?(path + '/text.txt')
-text = File.open(path + '/text.txt', "rb")
-contents = text.read.split(",")
-puts "Username: #{contents[0]}"
+path = File.expand_path File.dirname(__FILE__) #Finds Directory
+if File.exists?(path + '/text.txt') #Checks if text.txt exists
+text = File.open(path + '/text.txt', "rb") #if it does it opens it
+contents = text.read.split(",") #it changes its text to an array
+puts "Username: #{contents[0]}" #then spits it out
 puts "Password: #{contents[1]}"
 puts "Username: #{contents[2]}"
 puts "Password: #{contents[3]}"
-else
-print "Username1:"
-usr = gets.chomp
-print "Password1:"
+else #if it doesnt exist
+print "Username1:" #it asks for usr1
+usr = gets.chomp #gets it and chomps it
+print "Password1:" #and so on
 pwd = gets.chomp
 print "Username2:"
 usr2 = gets.chomp
 print "Password2:"
 pwd2 = gets.chomp
 
-savef = File.new(path + '/text.txt', "w")
-savef.print(usr + ',' + pwd + ',')
+savef = File.new(path + '/text.txt', "w") #it opens the file
+savef.print(usr + ',' + pwd + ',') #puts the text in with , seperating each
 savef.print(usr2 + ',' + pwd2)
-savef.close
-puts "file created"
+savef.close #closes file
+puts "file created" #done
 end
