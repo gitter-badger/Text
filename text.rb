da8f02c38c774c5ed7b2d4e8b6b921d4de5527e0
aoricm <aoricm@aol.com>
2014-11-01 16:53:38 +1100

    text

--- text.rb
+++ text.rb
@@ -1,24 +1,36 @@
-path = File.expand_path File.dirname(__FILE__) #Finds Directory
-if File.exists?(path + '/text.txt') #Checks if text.txt exists
-text = File.open(path + '/text.txt', "rb") #if it does it opens it
-contents = text.read.split(",") #it changes its text to an array
-puts "Username: #{contents[0]}" #then spits it out
-puts "Password: #{contents[1]}"
-puts "Username: #{contents[2]}"
-puts "Password: #{contents[3]}"
-else #if it doesnt exist
-print "Username1:" #it asks for usr1
-usr = gets.chomp #gets it and chomps it
-print "Password1:" #and so on
+def create
+path = File.expand_path File.dirname(__FILE__)
+print "Username:"
+usr = gets.chomp
+print "Password:"
 pwd = gets.chomp
-print "Username2:"
-usr2 = gets.chomp
-print "Password2:"
-pwd2 = gets.chomp
-
-savef = File.new(path + '/text.txt', "w") #it opens the file
-savef.print(usr + ',' + pwd + ',') #puts the text in with , seperating each
-savef.print(usr2 + ',' + pwd2)
-savef.close #closes file
-puts "file created" #done
+savef = File.new(path + '/text.txt', "a+")
+savef.print(usr + ',' + pwd + ',')
+savef.close
+puts "file created" 
+end
+def read
+path = File.expand_path File.dirname(__FILE__)
+text = File.open(path + '/text.txt', "rb") 
+contents = text.read.split(",")
+nxt = "y"
+u = 0
+p = 1
+while nxt === "y"
+puts "Username: #{contents[u]}"
+puts "Password: #{contents[p]}"
+print "Read Next?(y or n):"
+nxt = gets.chomp
+u += 2
+p += 2
+end
+end
+print "Create or Read (c or r):"
+opt = gets.chomp.downcase
+if opt === "c"
+create()
+elsif opt === "r"
+read()
+else
+puts "invalid option"
 end
