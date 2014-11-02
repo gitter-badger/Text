c4df9ddeda63c4ee537c34c0f253e87cda53a4d6
aoricm <aoricm@aol.com>
2014-11-02 15:43:51 +1100

    text

--- text.rb
+++ text.rb
@@ -1,22 +1,30 @@
 require 'open-uri'
-def userid(usr)
+def createusr(usr)
 path = File.expand_path File.dirname(__FILE__)
-source = open('http://xat.me/' + usr).read
+source = open('http://xat.me/' + usr.to_s).read
 source = source.match(/\?id\=(.*)\"\>Inappropriate\<\/A\>/m)[1].strip
 uid = source.match(/(.*)\&UserName/m)[1].strip
 savef = File.new(path + '/users.txt', "a+")
-savef.print("#{usr},")
+savef.print("#{usr.to_s},")
 savei = File.new(path + '/ids.txt', "a+")
 savei.print("#{uid},")
 savef.close
 savei.close
+puts "User Created!"
+end
+
+def findusr(usr)
+path = File.expand_path File.dirname(__FILE__)
+userid = File.open(path + '/users.txt', "rb") 
+userid = userid.read.split(",")
+puts "Bot Id:#{userid.index(usr)}"
 ids = File.open(path + '/ids.txt', "rb") 
 users = File.open(path + '/users.txt', "rb") 
 ids = ids.read.split(",")
 users = users.read.split(",")
-print "Bot File ID:"
-bfid = gets.chomp.to_i
-puts users[bfid]
-puts ids[bfid]
+puts "Username:#{users[userid.index(usr).to_i]}"
+puts "Xat Id:#{ids[userid.index(usr).to_i]}"
 end
-userid('pokemon')
+
+createusr('orianthi')
+findusr('orianthi')
